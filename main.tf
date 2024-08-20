d032f392e5b3e3996a4d21a447a7b7b89c3c9a14
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2024-08-20 10:11:47 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-resource "null_resource" "sleep" {
-  provisioner "local-exec" {
-    command = "sleep 240"
-  }
-  triggers = {
-    run_every_time = uuid()
-  }
+#resource "null_resource" "sleep" {
+#  provisioner "local-exec" {
+#    command = "sleep 240"
+#  }
+#  triggers = {
+#    run_every_time = uuid()
+#  }
+#}
+
+data "tfe_workspace" "test" {
+  name         = "1-concurrency-test"
+  organization = "georgi-berchev"
+}
+
+output "name" {
+  value = data.tfe_workspace.test.id
 }
